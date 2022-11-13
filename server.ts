3520137b2824256abdf40b3aa45c0e2ac87ae774
chandanmahto <chandanmahto00761@gmail.com>
2022-11-13 20:48:57 +0530

    + added prettier + added enums

--- server.ts
+++ server.ts
@@ -1,13 +1,36 @@
-import express, {Express} from "express";
+import express, { Express } from "express";
 import dotenv from "dotenv";
+import session from "express-session";
+import cookieParser from "cookie-parser";
+import {router as departmentRouter} from "./rest/department/router";
 
+// put .env file variables in process.env
 dotenv.config();
 
-const app:Express = express();
+const app: Express = express();
 const port = process.env.PORT;
+const sessionSecret = process.env.SESSION_SECRET;
 
 app.set("view engine", "jade");
 
+// setting up middlewares
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+app.use(
+  session({
+    secret: sessionSecret,
+    saveUninitialized: true,
+    cookie: {
+      maxAge: 1000 * 60 * 60 * 24, // one day
+    },
+    resave: false,
+  })
+);
+
+// setting up routers
+app.use("/department", departmentRouter);
+
 app.listen(port, () => {
-    console.log(`[SUCCESS] Server is running on http://localhost:${port}`);
+  console.log(`[SUCCESS] Server is running on http://localhost:${port}`);
 });
---
+++ utils/enums.ts
@@ -0,0 +1,19 @@
+export enum Month {
+  January = 1,
+  February,
+  March,
+  April,
+  May,
+  June,
+  July,
+  August,
+  September,
+  October,
+  November,
+  December,
+}
+
+export enum CourseType {
+  MTech,
+  PhD
+}
