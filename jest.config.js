fcc08da7effd385b1a3c400c8bb1bd5344cd548e
chandanmahto <chandanmahto00761@gmail.com>
2022-11-18 11:33:34 +0530

    + added github actions

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,18 @@
-/** @type {import('ts-jest').JestConfigWithTsJest} */
-
 // eslint-disable-next-line no-undef
 module.exports = {
-  preset: "ts-jest",
-  testEnvironment: "node",
+    preset: "ts-jest",
+    testEnvironment: "node",
+    testPathIgnorePatterns: [
+        "<rootDir>/node_modules",
+        "<rootDir>/repositories",
+    ],
+    coveragePathIgnorePatterns: [
+        "<rootDir>/node_modules",
+        "<rootDir>/repositories",
+    ],
+    coverageThreshold: {
+        global: {
+            lines: 80,
+        }
+    }
 };
